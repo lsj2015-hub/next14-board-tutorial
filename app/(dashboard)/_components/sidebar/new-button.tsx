a10c1f294a5e44446ac59e8918bed89f6a46166d
lsj2015-hub <lsj2015@hotmail.com>
2024-02-02 11:56:37 +0800

    Create Convex Schema & Api mutation func

--- app/(dashboard)/_components/sidebar/new-button.tsx
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -10,18 +10,18 @@ export const NewButton = () => {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <div className="aspect-square">
-          <Hint
-            label="Create organization"
-            side="right"
-            align="start"
-            sideOffset={18}
-          >
+        <Hint
+          label="Create organization"
+          side="right"
+          align="start"
+          sideOffset={18}
+        >
+          <div className="aspect-square">
             <button className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
               <Plus className="text-white" />
             </button>
-          </Hint>
-        </div>
+          </div>
+        </Hint>
       </DialogTrigger>
       <DialogContent className="p-0 bg-transparent border-none max-w-[480px]">
         <CreateOrganization />
