96586d735eeab8e88305702ccf7d7158b166223b
lsj2015-hub <lsj2015@hotmail.com>
2024-02-05 16:16:47 +0800

    Room Authentication

--- convex/board.ts
+++ convex/board.ts
@@ -1,6 +1,6 @@
 import { v } from 'convex/values';
 
-import { mutation } from './_generated/server';
+import { mutation, query } from './_generated/server';
 
 const images = [
   '/placeholders/1.svg',
@@ -144,6 +144,15 @@ export const unfavortite = mutation({
     if (!existingFavorite) throw new Error('Favorite not found');
 
     await ctx.db.delete(existingFavorite._id);
+
+    return board;
+  },
+});
+
+export const get = query({
+  args: { id: v.id('boards') },
+  handler: async (ctx, args) => {
+    const board = await ctx.db.get(args.id);
 
     return board;
   },
