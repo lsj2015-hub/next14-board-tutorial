97721fab934b1fc276af20dc95767eab0417d5d9
lsj2015-hub <lsj2015@hotmail.com>
2024-02-09 20:54:56 +0800

    Insert layer

---
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { memo } from 'react';
+
+import { useStorage } from '@/liveblocks.config';
+import { LayerType } from '@/types/canvas';
+import { Rectangle } from './rectangle';
+
+interface LayerPreviewProps {
+  id: string;
+  onLayerPointerDown: () => void; // TODO: Fix types
+  selectionColor?: string;
+}
+
+export const LayerPreview = memo(
+  ({ id, onLayerPointerDown, selectionColor }: LayerPreviewProps) => {
+    const layer = useStorage((root) => root.layers.get(id));
+
+    // console.log({ layer }, 'LAYER_REVIEW');
+
+    if (!layer) return null;
+
+    switch (layer.type) {
+      case LayerType.Rectangle:
+        return (
+          <Rectangle
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
+        );
+
+      default:
+        console.warn('Unknown layer type');
+        return null;
+    }
+  }
+);
+
+LayerPreview.displayName = 'LayerPreview';
