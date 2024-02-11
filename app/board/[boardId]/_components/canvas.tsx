c0e02dc66c354d9529b5495d2e7db19d6552243c
lsj2015-hub <lsj2015@hotmail.com>
2024-02-11 17:50:43 +0800

    Color change & Deletion

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -34,6 +34,7 @@ import { Toolbar } from './toolbar';
 import { CursorsPresence } from './cursors-presence';
 import { LayerPreview } from './layer-preview';
 import { SelectionBox } from './selection-box';
+import { SelectionTools } from './selection-tools';
 
 const MAX_LAYERS = 100;
 
@@ -284,6 +285,7 @@ export const Canvas = ({ boardId }: CanvasProps) => {
         undo={history.undo}
         redo={history.redo}
       />
+      <SelectionTools camera={camera} setLastUsedColor={setLastUsedColor} />
       <svg
         className="h-[100vh] w-[100vw]"
         onWheel={onWheel}
